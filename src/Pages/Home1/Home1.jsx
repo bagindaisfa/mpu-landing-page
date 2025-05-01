40f4f41792c6dbf88b0addabc9b7247ab46c57b4
bagindaQuadrant <bpulungan@appen.com>
2025-05-01 22:48:58 +0700

    update 1

--- src/Pages/Home1/Home1.jsx
+++ src/Pages/Home1/Home1.jsx
@@ -24,9 +24,9 @@ const Home1 = () => {
       <Counter />
       <Work />
       {/* <Pricing /> */}
-      <LatestWork />
-      <TeamMember />
-      <Testimonial />
+      {/* <LatestWork /> */}
+      {/* <TeamMember /> */}
+      {/* <Testimonial /> */}
       <Blog />
     </>
   );
