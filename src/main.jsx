595afe432acfaa8916f18ba50f22553497c7ea0a
bagindaQuadrant <bpulungan@appen.com>
2025-05-02 09:44:15 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { HelmetProvider } from 'react-helmet-async';
 import Preloader from './Shared/Preloader/Preloader';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
-import router from './Router/router';
+import router from './Router/Router';
 import MaintenancePage from './Component1Default/MaintenancePage/MaintenancePage';
 
 const helmetContext = {}; // Define helmetContext here
@@ -20,7 +20,7 @@ root.render(
     ) : (
       <>
         {/* Ini bagian aslinya yang normal */}
-        <HelmetProvider>
+        <HelmetProvider context={helmetContext}>
           <Preloader />
           <RouterProvider router={router} />
         </HelmetProvider>
